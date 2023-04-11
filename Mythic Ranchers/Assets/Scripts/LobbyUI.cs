81ba14794c442eca390268889a1b4a9ead679574
Matei Pelletier <94388648+Blasayyy@users.noreply.github.com>
2023-04-10 20:28:44 -0400

    Created LobbyUI.cs

---
+++ Mythic Ranchers/Assets/Scripts/LobbyUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Authentication;
+using Unity.Services.Lobbies.Models;
+using UnityEngine.UI;
+using TMPro;
+
+public class LobbyUI : MonoBehaviour
+{
+    public static LobbyUI Instance { get; private set; }
+}
