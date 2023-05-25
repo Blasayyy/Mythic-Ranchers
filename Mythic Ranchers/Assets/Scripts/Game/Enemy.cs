d3507ec20dfbcf6802b4c912661b716592c40fd9
Chris <74682475+Whuttz@users.noreply.github.com>
2023-05-25 11:53:41 -0400

    added enemy flicker on damage

--- Mythic Ranchers/Assets/Scripts/Game/Enemy.cs
+++ Mythic Ranchers/Assets/Scripts/Game/Enemy.cs
@@ -19,6 +19,10 @@ public class Enemy : NetworkBehaviour
     public HealthBar healthBar;
     public RessourceBar ressourceBar;
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    public float flickerDuration = 0.1f;
+    public int flickerCount = 2;
+    private Color flickerColor = Color.red;
 
     public enum EnemyState
     {
@@ -26,7 +30,6 @@ public class Enemy : NetworkBehaviour
         Chasing,
         Idle
     }
-
 
     private void Awake()
     {
@@ -36,6 +39,7 @@ public class Enemy : NetworkBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         currentHp = maxHp;
         currentRessource = maxRessource;
         healthBar.SetHealth(currentHp, maxHp);
@@ -62,6 +66,18 @@ public class Enemy : NetworkBehaviour
             StartCoroutine(Wander());
         }
 
+    }
+
+    IEnumerator DamageFlicker()
+    {
+        for (int i = 0; i < flickerCount; i++)
+        {
+            spriteRenderer.color = flickerColor;
+            yield return new WaitForSeconds(flickerDuration);
+
+            spriteRenderer.color = Color.white;
+            yield return new WaitForSeconds(flickerDuration);
+        }
     }
 
     IEnumerator Wander()
@@ -112,6 +128,7 @@ public class Enemy : NetworkBehaviour
     public void LoseHealth(float amount)
     {
         CurrentHp -= amount;
+        StartCoroutine(DamageFlicker());
     }
 
     public void LoseRessource(float amount)
